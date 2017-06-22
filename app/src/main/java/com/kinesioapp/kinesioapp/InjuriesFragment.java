fd97a33452a1b6013603779dfce5cf96f273d287
Sami Niemi <sami.niemi@live.fi>
2017-06-22 23:29:46 +0300

    Created togglebutton that changes it's icon when pressed, changed launcer icon, changed underline color of selected tab in frontpage.

--- app/src/main/java/com/kinesioapp/kinesioapp/InjuriesFragment.java
+++ app/src/main/java/com/kinesioapp/kinesioapp/InjuriesFragment.java
@@ -5,11 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.Locale;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
+
 public class InjuriesFragment extends Fragment {
+
+    @BindView(R.id.favoriteButtonId) ToggleButton favoriteButton;
+
+    private String favoriteOne;
+
     public static final String INJURY_NUMBER = "planet_number";
 
     public InjuriesFragment() {
@@ -20,6 +33,8 @@ public class InjuriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_injuries, container, false);
+        ButterKnife.bind(this, rootView);
+
         int i = getArguments().getInt(INJURY_NUMBER);
         String injury = getResources().getStringArray(R.array.menu_array)[i];
 
@@ -27,7 +42,18 @@ public class InjuriesFragment extends Fragment {
                 "drawable", getActivity().getPackageName());
         ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
         getActivity().setTitle(injury);
+
         return rootView;
+    }
+
+    @OnCheckedChanged(R.id.favoriteButtonId)
+    public void toggleFavorite(boolean isChecked) {
+        if (isChecked) {
+            Toast.makeText(getActivity(),"REMOVE!",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity(),"ADD!",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
